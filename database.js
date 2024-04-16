5b9e44daa45fb8886e10a7bfbdb692dc18b39a3f
water2027 <136900643+water2027@users.noreply.github.com>
2024-04-16 13:06:20 +0800

    Update database.js

--- database.js
+++ database.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: 'localhost', // 数据库服务器地址
-    user: 'water', // 数据库用户名
-    database: 'boke', // 数据库名
-    password: '@2004LSYqs' // 数据库密码
+    user: '', // 数据库用户名
+    database: '', // 数据库名
+    password: '' // 数据库密码
 });
 
 module.exports = pool.promise();
